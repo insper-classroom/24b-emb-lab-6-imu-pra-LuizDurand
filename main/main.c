80fe0de64a64da78a7f513bf0ebadb4ff486b977
Luiz <luizfbd@al.insper.edu.br>
2024-10-09 13:22:01 -0300

    lab 6 feito

--- main/main.c
+++ main/main.c
@@ -2,6 +2,7 @@
 #include <task.h>
 #include <semphr.h>
 #include <queue.h>
+#include <math.h> 
 
 #include "pico/stdlib.h"
 #include <stdio.h>
@@ -15,6 +16,13 @@
 const int MPU_ADDRESS = 0x68;
 const int I2C_SDA_GPIO = 4;
 const int I2C_SCL_GPIO = 5;
+
+typedef struct Mouse {
+    int axis;
+    int val;
+} mouse;
+
+QueueHandle_t xQueueAdc;
 
 static void mpu6050_reset() {
     // Two byte reset. First byte register, second byte data
@@ -59,6 +67,7 @@ static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
 }
 
 void mpu6050_task(void *p) {
+    mouse mouse;
     i2c_init(i2c_default, 400 * 1000);
     gpio_set_function(I2C_SDA_GPIO, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SCL_GPIO, GPIO_FUNC_I2C);
@@ -68,11 +77,47 @@ void mpu6050_task(void *p) {
     mpu6050_reset();
     int16_t acceleration[3], gyro[3], temp;
 
+    FusionAhrs ahrs;
+    FusionAhrsInitialise(&ahrs);
+
+    // Define sample period
+    #define SAMPLE_PERIOD (0.01f) // 10 ms
+
     while(1) {
         mpu6050_read_raw(acceleration, gyro, &temp);
-        printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
-        printf("Gyro. X = %d, Y = %d, Z = %d\n", gyro[0], gyro[1], gyro[2]);
-        printf("Temp. = %f\n", (temp / 340.0) + 36.53);
+
+        // Convert raw data to physical units
+        FusionVector gyroscope;
+        gyroscope.axis.x = gyro[0] / 131.0f;   // Convert to degrees/s
+        gyroscope.axis.y = gyro[1] / 131.0f;
+        gyroscope.axis.z = gyro[2] / 131.0f;
+
+        FusionVector accelerometer;
+        accelerometer.axis.x = acceleration[0] / 16384.0f;  // Convert to g
+        accelerometer.axis.y = acceleration[1] / 16384.0f;
+        accelerometer.axis.z = acceleration[2] / 16384.0f;
+
+        // Update FusionAhrs without magnetometer data
+        FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, SAMPLE_PERIOD);
+
+        // Get Euler angles from the quaternion
+        FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
+
+        if(fabs(euler.angle.roll) >= 10){
+            mouse.axis = 1;
+            mouse.val = -euler.angle.roll;
+            xQueueSend(xQueueAdc, &mouse, portMAX_DELAY);
+        }
+        if(fabs(euler.angle.yaw) >= 10){
+            mouse.axis = 0;
+            mouse.val = -euler.angle.yaw;
+            xQueueSend(xQueueAdc, &mouse, portMAX_DELAY);
+        }
+        if(fabs(accelerometer.axis.y) >= 1.5){
+            mouse.axis = 2;
+            mouse.val = 1;
+            xQueueSend(xQueueAdc, &mouse, portMAX_DELAY);
+        }
 
         vTaskDelay(pdMS_TO_TICKS(10));
     }
@@ -80,10 +125,30 @@ void mpu6050_task(void *p) {
 
 }
 
+void uart_task(void *p) {
+    mouse mouse;
+
+    while (1) {
+        if(xQueueReceive(xQueueAdc, &mouse, portMAX_DELAY)){
+            uart_putc(uart0, mouse.axis);
+            uart_putc(uart0, mouse.val >> 8);
+            uart_putc(uart0, mouse.val & 0xFF);
+            uart_putc(uart0, -1);
+        }
+    }
+}
+
 int main() {
     stdio_init_all();
 
+    uart_init(uart0, 115200);
+    gpio_set_function(0, GPIO_FUNC_UART);
+    gpio_set_function(1, GPIO_FUNC_UART);
+
+    xQueueAdc = xQueueCreate(32, sizeof(mouse));
+
     xTaskCreate(mpu6050_task, "mpu6050_Task 1", 8192, NULL, 1, NULL);
+    xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
